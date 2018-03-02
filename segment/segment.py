a202c6b07195bc95045fc987b7832b596c837a83
Amit <camitiw@gmail.com>
2018-03-02 23:46:34 +0545

    Follow PEP8 formatting

--- segment/segment.py
+++ segment/segment.py
@@ -1,49 +1,52 @@
 import os
-import numpy as np
 import cv2
 
-# Segment video into respective frames.
-def extractImages(pathIn, pathOut):
-  print ("Segmenting....")
-  os.makedirs(pathOut, exist_ok=True) # Replace Path with your desired Path.
-  vidcap = cv2.VideoCapture(pathIn)
-  success,image = vidcap.read()
-  count = 0
-  while success:
-    cv2.imwrite( pathOut + "frame%d.png" % count, image)
-    success,image = vidcap.read()
-    count += 1
-  vidcap.release()
 
-sampleVideo = 'to_segment.mp4'
+# Segment video into respective frames.
+def extract_images(pathIn, pathOut):
+    print("Segmenting....")
+    os.makedirs(pathOut, exist_ok=True)  # Replace Path with your desired Path.
+    vidcap = cv2.VideoCapture(pathIn)
+    success, image = vidcap.read()
+    count = 0
+    while success:
+        cv2.imwrite(pathOut + "frame%d.png" % count, image)
+        success, image = vidcap.read()
+        count += 1
+    vidcap.release()
+
+
+sample_video = 'to_segment.mp4'
 dest_path = './frames/'
-extractImages(sampleVideo, dest_path) # Choose your PathOut yourself.
+extract_images(sample_video, dest_path)  # Choose your PathOut yourself.
+
 
 def extract_fps(path):
-  vidcap = cv2.VideoCapture(path)
-  fps = vidcap.get(cv2.CAP_PROP_FPS)
-  return fps
+    vidcap = cv2.VideoCapture(path)
+    fps = vidcap.get(cv2.CAP_PROP_FPS)
+    return fps
 
 
-#Combine respective frames into a video.
+# Combine respective frames into a video
 def frames_to_video(pathIn,pathOut,fps):
-   print ('Combining...')
-   image_array = []
-   # Array of video frames path eg:'/home/kamal/Desktop/major-project/frames/frame1.png'
-   files = [f for f in os.listdir(pathIn) if os.path.isfile(pathIn + f)] 
-   files.sort(key = lambda x: int(x[5:-4]))
-   for file in files:
+    print('Combining...')
+    image_array = []
+    # Array of video frames path eg:'/home/kamal/Desktop/major-project/frames/frame1.png'
+    files = [f for f in os.listdir(pathIn) if os.path.isfile(pathIn + f)]
+    files.sort(key=lambda x: int(x[5:-4]))
+    for file in files:
        img = cv2.imread(pathIn + file)
        size =  (img.shape[1],img.shape[0])
        img = cv2.resize(img,size,interpolation = cv2.INTER_CUBIC)
        image_array.append(img)
-   fourcc = cv2.VideoWriter_fourcc('D', 'I', 'V', 'X')
-   out = cv2.VideoWriter(pathOut,fourcc, fps, size)
-   for image in image_array:
+    fourcc = cv2.VideoWriter_fourcc('D', 'I', 'V', 'X')
+    out = cv2.VideoWriter(pathOut, fourcc, fps, size)
+    for image in image_array:
        out.write(image)
-   out.release()
+    out.release()
+
 
 pathIn = './frames/'
 pathOut =  'joined_video.mp4'
-fps = extract_fps(sampleVideo) #this fps is evaluated in extractImage method.
+fps = extract_fps(sample_video)  # this fps is evaluated in extractImage method.
 frames_to_video(pathIn,pathOut,fps)
