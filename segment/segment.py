2cda632fc5b811d760d613bde23e500e50949adc
Amit <camitiw@gmail.com>
2018-03-02 23:36:24 +0545

    Fix extra image getting generated

--- segment/segment.py
+++ segment/segment.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 import cv2
 
-#Segment Video into respective frames.
+# Segment video into respective frames.
 def extractImages(pathIn, pathOut):
   print ("Segmenting....")
   os.makedirs(pathOut, exist_ok=True) # Replace Path with your desired Path.
@@ -10,11 +10,9 @@ def extractImages(pathIn, pathOut):
   fps = vidcap.get(cv2.CAP_PROP_FPS)
   success,image = vidcap.read()
   count = 0
-  success = True
   while success:
-    success,image = vidcap.read()
-    #print ('Read a new frame: ', success) #check whether the video is segmented for each frame.
     cv2.imwrite( pathOut + "frame%d.png" % count, image)
+    success,image = vidcap.read()
     count += 1
   vidcap.release()
 
@@ -27,17 +25,18 @@ extractImages(sampleVideo, dest_path) # Choose your PathOut yourself.
 def frames_to_video(pathIn,pathOut,fps):
    print ('Combining...')
    image_array = []
-   files = [f for f in os.listdir(pathIn) if os.path.isfile(pathIn + f)] #array of video frames path eg:'/home/kamal/Desktop/major-project/frames/frame1.png'
+   # Array of video frames path eg:'/home/kamal/Desktop/major-project/frames/frame1.png'
+   files = [f for f in os.listdir(pathIn) if os.path.isfile(pathIn + f)] 
    files.sort(key = lambda x: int(x[5:-4]))
-   for i in range(len(files)-1):
-       img = cv2.imread(pathIn + files[i])
+   for file in files:
+       img = cv2.imread(pathIn + file)
        size =  (img.shape[1],img.shape[0])
        img = cv2.resize(img,size,interpolation = cv2.INTER_CUBIC)
        image_array.append(img)
    fourcc = cv2.VideoWriter_fourcc('D', 'I', 'V', 'X')
    out = cv2.VideoWriter(pathOut,fourcc, fps, size)
-   for i in range(len(image_array)):
-       out.write(image_array[i])
+   for image in image_array:
+       out.write(image)
    out.release()
 
 pathIn = './frames/'
