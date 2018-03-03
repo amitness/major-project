bfe6114226c513f2ebef0e4d42c329365157390c
kamalpaneru <kamalpaneru.15@gmail.com>
2018-03-03 14:38:52 +0545

    Remove old commented code

--- segment/segment.py
+++ segment/segment.py
@@ -1,16 +1,15 @@
 import os
 import cv2
 
+
 # Segment video into respective frames.
 def extract_frames(pathIn):
     print("Extracting frames from video ...")
-    #os.makedirs(pathOut, exist_ok=True)
     vidcap = cv2.VideoCapture(pathIn)
     fps = vidcap.get(cv2.CAP_PROP_FPS)
     success, frame = vidcap.read()
     frames = []
     while success:
-        # cv2.imwrite(pathOut + "frame%d.png" % count, image)
         frames.append(frame)
         success, frame = vidcap.read()
     vidcap.release()
@@ -35,14 +34,6 @@ def scale_frames(frames, scaleX=4, scaleY=4):
 # Combine respective frames into a video
 def frames_to_video(frames, fps, pathOut="./out.mp4"):
     print('Combining frames')
-    # Array of video frames path eg:'/home/kamal/Desktop/major-project/frames/frame1.png'
-    # files = [f for f in os.listdir(pathIn) if os.path.isfile(pathIn + f)]
-    # files.sort(key=lambda x: int(x[5:-4]))
-    # for file in files:
-    #    img = cv2.imread(pathIn + file)
-    #    size =  (img.shape[1],img.shape[0])
-    #    img = cv2.resize(img,size,interpolation = cv2.INTER_CUBIC)
-    #    image_array.append(img)
     fourcc = cv2.VideoWriter_fourcc('D', 'I', 'V', 'X')
     out = cv2.VideoWriter(pathOut, fourcc, fps, frames[0].shape[:2][::-1])
     for frame in frames:
@@ -54,13 +45,8 @@ def scale_video(video_location, dest_path="./out.mp4", scaleX=2, scaleY=2):
     scaled_frames = scale_frames(frames, scaleX, scaleY)
     frames_to_video(scaled_frames, fps, dest_path)
 
+
 if __name__ == '__main__':
     video_location = 'to_segment.mp4'
     scale_video(video_location, dest_path="./out.mp4", scaleX=2, scaleY=2)
-    # dest_path = './frames/'
-    # fps, frames = extract_frames(video_location)  # Choose your PathOut yourself.
-    # scaled_frames = scale_frames(frames)
-    # pathIn = './frames/'
-    # pathOut =  'joined_video.mp4'
-    # fps = extract_fps(sample_video)  # this fps is evaluated in extractImage method.
-    # frames_to_video(scaled_frames, fps)
+
