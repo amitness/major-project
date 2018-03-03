3d246444d3a299d90270dc9356e36cc468980bd0
kamalpaneru <kamalpaneru.15@gmail.com>
2018-03-03 14:59:19 +0545

    Remove CamelCase and replace with snake_case for entire codebase

--- segment/segment.py
+++ segment/segment.py
@@ -22,12 +22,12 @@ def extract_fps(path):
     fps = vidcap.get(cv2.CAP_PROP_FPS)
     return fps
 
-def scale_frames(frames, scaleX=4, scaleY=4):
+def scale_frames(frames, scale_x=4, scale_y=4):
     print("Scaling images")
     scaled_frames = []
     for frame in frames:
        size =  (frame.shape[1],frame.shape[0])
-       scaled_frame = cv2.resize(frame, (0,0), fx=scaleX, fy=scaleY, interpolation=cv2.INTER_CUBIC)
+       scaled_frame = cv2.resize(frame, (0,0), fx=scale_x, fy=scale_y, interpolation=cv2.INTER_CUBIC)
        scaled_frames.append(scaled_frame)
     return scaled_frames
 
@@ -42,11 +42,11 @@ def frames_to_video(frames, fps, pathOut="./out.mp4"):
 
 def scale_video(video_location, dest_path="./out.mp4", scale_x=2, scale_y=2):
     fps, frames = extract_frames(video_location)  # Choose your PathOut yourself.
-    scaled_frames = scale_frames(frames, scaleX, scaleY)
+    scaled_frames = scale_frames(frames, scale_x, scale_y)
     frames_to_video(scaled_frames, fps, dest_path)
 
 
 if __name__ == '__main__':
     video_location = 'to_segment.mp4'
-    scale_video(video_location, dest_path="./out.mp4", scaleX=2, scaleY=2)
+    scale_video(video_location, dest_path="./out.mp4", scale_x=2, scale_y=2)
 
