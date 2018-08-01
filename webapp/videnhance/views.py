4a09dc5a15d670dd2c9f1b1c9ad89fd1d8c0d4c7
Amit Chaudhary <camitiw@gmail.com>
2018-08-02 01:55:40 +0545

    Test slider

--- webapp/videnhance/views.py
+++ webapp/videnhance/views.py
@@ -14,10 +14,13 @@ def save_video(request):
         filename = fs.save(uploaded_file.name, uploaded_file)
         # TODO: THis url will be used in future to give user download link
         url = fs.url(filename)
-        full_url = request.build_absolute_uri(url)
+        # full_url = request.build_absolute_uri(url)
+        full_url = 'http://127.0.0.1:8000/media/input.mp4'
+        full_url2 = 'http://127.0.0.1:8000/media/cnn.mp4'
         import time
         time.sleep(2)
         # return redirect(full_url)
         # return HttpResponse(template)
-        return render(request, 'video.html', context={'video_url': full_url})
+        # return render(request, 'video.html', context={'video_url': full_url})
+        return render(request, 'video2.html', context={'video_url': full_url, 'video_url2': full_url2})
 
