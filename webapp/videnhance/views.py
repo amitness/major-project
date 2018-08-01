c2038debd2023fe5e29295a2a93411c468330862
Amit Chaudhary <camitiw@gmail.com>
2018-08-01 11:11:31 +0545

    Revamp the interface

--- webapp/videnhance/views.py
+++ webapp/videnhance/views.py
@@ -1,6 +1,6 @@
+from django.core.files.storage import FileSystemStorage
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from django.core.files.storage import FileSystemStorage
 
 
 def home(request):
@@ -9,11 +9,15 @@ def home(request):
 
 def save_video(request):
     if request.method == 'POST':
-        uploaded_file = request.FILES['file']
+        uploaded_file = request.FILES['fileUpload']
         fs = FileSystemStorage()
         filename = fs.save(uploaded_file.name, uploaded_file)
         # TODO: THis url will be used in future to give user download link
         url = fs.url(filename)
         full_url = request.build_absolute_uri(url)
+        import time
+        time.sleep(2)
         # return redirect(full_url)
-        return HttpResponse("Done")
+        # return HttpResponse(template)
+        return render(request, 'video.html', context={'video_url': full_url})
+
